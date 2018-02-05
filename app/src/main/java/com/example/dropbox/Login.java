f1a640bac77aad0046ac8445df34fc98e5b2f1cb
MikovIvan <mikov_ivan@list.ru>
2018-02-05 21:14:02 +0300

    add ClientTask, check email and password

--- app/src/main/java/com/example/dropbox/Login.java
+++ app/src/main/java/com/example/dropbox/Login.java
@@ -1,6 +1,7 @@
 package com.example.dropbox;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,79 +9,85 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+import static com.example.dropbox.Constants.PORT;
+import static com.example.dropbox.Constants.SERVER_IP;
+
 
 public class Login extends AppCompatActivity {
 
-    private EditText email;
-    private EditText password;
+    EditText email;
+    EditText password;
     private Button btnLogin;
     private TextView registration;
-
-    private final int PORT = 8189;
-    private final String SERVER_IP = "localhost";
-    private Socket socket;
-    private DataInputStream in;
-    private DataOutputStream out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//       new Thread(new com.example.dropbox.connection.Connection()).start();
-
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         btnLogin = findViewById(R.id.button_login);
         registration = findViewById(R.id.registration);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                auth();
+                ClientTask clientTask = new ClientTask(SERVER_IP, PORT, email.getText().toString() + password.getText().toString());
+                clientTask.execute();
 
+            }
+        });
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Registration.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void onClick(View view) {
-        if (view.equals(R.id.registration)) {
-            Intent intent = new Intent(Login.this, Registration.class);
-            startActivity(intent);
+    public class ClientTask extends AsyncTask<String, Void, String> {
+        String dstIP;
+        int dstPort;
+
+        Socket socket = null;
+        DataOutputStream out = null;
+        DataInputStream in = null;
+
+        public ClientTask(String IP, int port, String s) {
+            this.dstIP = IP;
+            this.dstPort = port;
         }
-    }
 
+        @Override
+        protected String doInBackground(String... params) {
+            String serverMessage = "Server message";
 
-    public void start() {
-
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    try {
-                        socket = new Socket(SERVER_IP, PORT);
-                        in = new DataInputStream(socket.getInputStream());
-                        out = new DataOutputStream(socket.getOutputStream());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    while (true) {
-                        String msg = in.readUTF();
-                        if (msg.startsWith("/authok")) {
-                            Intent intent = new Intent(Login.this, MainActivity.class);
-                            startActivity(intent);
-                            break;
-                        }
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
+            try {
+                socket = new Socket(SERVER_IP, PORT);
+                out = new DataOutputStream(socket.getOutputStream());
+                in = new DataInputStream(socket.getInputStream());
+                out.writeUTF("/auth" + " " + email.getText().toString() + " " + password.getText().toString());
+                serverMessage = in.readUTF();
+                if (serverMessage.startsWith("/authok")) {
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                } else if(serverMessage.startsWith("/authfailed")){
+                    Toast.makeText(Login.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
                     try {
                         socket.close();
                     } catch (IOException e) {
@@ -88,22 +95,7 @@ public class Login extends AppCompatActivity {
                     }
                 }
             }
-        });
-        thread1.start();
-    }
-
-    public void auth() {
-        if (socket == null || socket.isClosed()) start();
-        try {
-            Log.v("EditText", email.getText().toString());
-            Log.v("EditText", password.getText().toString());
-            out.writeUTF("/auth " + email.getText() + " " + password.getText());
-            email.setText("");
-            password.setText("");
-        } catch (IOException e) {
-            e.printStackTrace();
+            return serverMessage;
         }
     }
-
-
 }
