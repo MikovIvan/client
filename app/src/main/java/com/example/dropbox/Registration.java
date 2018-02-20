778cf453018ab0396bbbb8a0906387c75ba15951
MikovIvan <mikov_ivan@list.ru>
2018-02-20 12:07:33 +0300

    make registration

--- app/src/main/java/com/example/dropbox/Registration.java
+++ app/src/main/java/com/example/dropbox/Registration.java
@@ -1,10 +1,21 @@
 package com.example.dropbox;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static com.example.dropbox.constants.Constants.PORT;
+import static com.example.dropbox.constants.Constants.SERVER_IP;
 
 public class Registration extends AppCompatActivity {
 
@@ -13,6 +24,7 @@ public class Registration extends AppCompatActivity {
     private EditText regEmail;
     private EditText regPassword;
     private EditText regConfirmPassword;
+    private Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +36,67 @@ public class Registration extends AppCompatActivity {
         regEmail = findViewById(R.id.reg_email);
         regPassword = findViewById(R.id.reg_password);
         regConfirmPassword = findViewById(R.id.reg_confirmpassword);
+        regBtn = findViewById(R.id.button_registration);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClientTask clientTask = new ClientTask(SERVER_IP, PORT, regName.getText().toString() + regSurname.getText().toString()+
+                        regEmail.getText().toString()+regPassword.getText().toString()+regConfirmPassword.getText().toString());
+                clientTask.execute();
+
+
+            }
+        });
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(Registration.this, Login.class );
-        startActivity(intent);
+    public class ClientTask extends AsyncTask<String, Void, String> {
+        String dstIP;
+        int dstPort;
+
+        Socket socket = null;
+        DataOutputStream out = null;
+        DataInputStream in = null;
+
+        public ClientTask(String IP, int port, String s) {
+            this.dstIP = IP;
+            this.dstPort = port;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String serverMessage = "Server message";
+
+            try {
+                socket = new Socket(SERVER_IP, PORT);
+                out = new DataOutputStream(socket.getOutputStream());
+                in = new DataInputStream(socket.getInputStream());
+                out.writeUTF("/reg" + " " + regName.getText().toString()+ " " + regSurname.getText().toString()+ " "+
+                        regEmail.getText().toString()+ " "+regPassword.getText().toString());
+                serverMessage = in.readUTF();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return serverMessage;
+        }
+
+        @Override
+        protected void onPostExecute(String serverMessage) {
+            super.onPostExecute(serverMessage);
+            if(serverMessage.startsWith("/regok")){
+                Intent intent = new Intent(Registration.this, Login.class );
+                startActivity(intent);
+            } else if(serverMessage.startsWith("/regfailed")){
+                Toast.makeText(Registration.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
