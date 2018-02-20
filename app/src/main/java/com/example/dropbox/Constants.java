0f19164eabde4bdad8eccf450c7a6bc5c397a9e3
MikovIvan <mikov_ivan@list.ru>
2018-02-20 12:04:59 +0300

    remove to another package

--- app/src/main/java/com/example/dropbox/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.dropbox;
-
-
-public final class Constants {
-    public static final int PORT = 8888;
-    public static final String SERVER_IP = "192.168.100.4";
-
-}
---
+++ app/src/main/java/com/example/dropbox/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.example.dropbox.constants;
+
+
+public final class Constants {
+    public static final int PORT = 8888;
+    public static final String SERVER_IP = "192.168.100.6";
+
+}
