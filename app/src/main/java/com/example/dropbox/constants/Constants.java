57c33f8d8c7d7d67005c33e75ccd6a1c457d5bb6
MikovIvan <mikov_ivan@list.ru>
2018-02-26 23:06:07 +0300

    update

--- app/src/main/java/com/example/dropbox/constants/Constants.java
+++ app/src/main/java/com/example/dropbox/constants/Constants.java
@@ -3,6 +3,6 @@ package com.example.dropbox.constants;
 
 public final class Constants {
     public static final int PORT = 8888;
-    public static final String SERVER_IP = "192.168.100.6";
+    public static final String SERVER_IP = "192.168.1.5";
 
 }
